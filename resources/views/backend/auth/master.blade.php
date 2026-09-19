e1aa5da6f437451b0f5799c067ee29ae25f02164
FreelancerArif1 <58873534+FreelancerArif1@users.noreply.github.com>
2026-09-19 15:22:45 +0600

    c

--- resources/views/backend/auth/master.blade.php
+++ resources/views/backend/auth/master.blade.php
@@ -8,6 +8,29 @@
     <link rel="icon" type="image/png" href="{{ asset(Helper::getSettings('site_favicon')) }}?v={{ time() }}">
     <link rel="apple-touch-icon" href="{{ asset(Helper::getSettings('site_favicon')) }}?v={{ time() }}">
 
+
+    <style>
+        .loging_page_logo{
+    height: 100px;
+    width: 100px;
+    background: #fff;
+    border-radius: 50%;
+    margin: 0 auto;
+    border: 1px solid #ca9f3a;
+    padding: 5px;
+}
+.loging_page_logo img{
+    object-fit: contain;
+}
+.login-form a, .login-form p{
+    color: #dbb652 !important;
+}
+ .btn-primary:hover, .login-form .btn {
+    color: #fff !important;
+    background: #dbb652 !important;
+    border-color: #dbb652 !important;
+}
+    </style>
 </head>
 
 <body class="login">
